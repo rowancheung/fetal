d80a18341ff9b7cfefbf5ec78c78e26527b670fe
Rowan <rtcheung@mit.edu>
2019-03-13 19:55:16 -0400

    dilate mask

--- registration.py
+++ registration.py
@@ -1,13 +1,13 @@
 
-from constants import USABLE_SAMPLES
-import pandas as pd
+from constants import greater_95
+# import pandas as pd
 import os
 import subprocess
 import util
 import numpy as np
 import scipy.ndimage as sp
 
-samples = pd.read_csv('slices.csv', index_col = 0)
+# samples = pd.read_csv('slices.csv', index_col = 0)
 
 labelled_frames = {}
 
@@ -20,38 +20,46 @@ def get_frame_num(s):
   else:
     return '0' + s
 
-for s in USABLE_SAMPLES:
-  labelled_frames[s] = get_frame_num(int(samples.loc[s]['labeled frame']))
+# for s in USABLE_SAMPLES:
+#   labelled_frames[s] = get_frame_num(int(samples.loc[s]['labeled frame']))
 
-g_truth_dir = '../data/labels'
+g_truth_dir = '../../placenta_segmentation/data/labels'
 registration_dir = './registered/'
-data_dir = '../data/raw/raw/'
-seg_dir = '../data/data'
+data_dir = '../../placenta_segmentation/data/raw/'
+# data_dir = '../data/raw/'
+seg_dir = '../../placenta_segmentation/data/predict_cleaned'
 command = '../ITK_4D/build/bin/TemporalRegistration_Rigid'
 
 def union(m1, m2):
   return np.ma.mask_or(m1, m2)
 
-def dilate(mask, iterations=1):
-  struct = sp.generate_binary_structure(2, 1)
-  return sp.binary_dilation(mask, structure=struct, iterations=iterations)
+def dilate(mask, sample, iterations=4):
+  struct = sp.generate_binary_structure(3, 1)
+  dil = sp.binary_dilation(mask, iterations=iterations)
+  util.save_vol(dil, '{}mask/{}.nii.gz'.format(registration_dir, sample))
+  return dil
 
 def register(sample):
-  g_truth_frame = labelled_frames[sample]
+  # g_truth_frame = labelled_frames[sample]
   all_frames = os.listdir(os.path.join(data_dir, sample))
 
   for i in range(len(all_frames)):
     all_frames[i] = os.path.join(os.path.join(data_dir, sample), all_frames[i])
 
-  mask = '{}/{}/{}_{}_all_brains.nii.gz'.format(g_truth_dir, sample, sample, g_truth_frame)
-  commands = [command, registration_dir, len(all_frames), mask, '1', '0']
+  mask1 = '{}/021218L/021218L_0180_all_brains.nii.gz'.format(g_truth_dir)
+  mask2 = '{}/021218L/021218L_0180.nii.gz'.format(seg_dir)
+  # mask1 = util.read_vol('../data/labels/021218L/021218L_0180_all_brains.nii.gz')
+  # mask2 = util.read_vol('../data/data/021218L_0180.nii.gz')
+  u = union(mask1, mask2)
+  dil = dilate(u, sample)
+  commands = [command, registration_dir, len(all_frames), './registered/mask/021218L.nii.gz', '1', '0']
   back_commands = commands.copy()
-  prev_frames = all_frames[:int(g_truth_frame)]
+  prev_frames = all_frames[:int(180) + 1]
   prev_frames.reverse()
   back_commands.extend(prev_frames)
-  commands.extend(all_frames[int(g_truth_frame):])
-  back_commands[2] = str(int(g_truth_frame))
-  commands[2] = str(len(all_frames) - int(g_truth_frame))
+  commands.extend(all_frames[int(180):])
+  back_commands[2] = str(int(180))
+  commands[2] = str(len(all_frames) - int(180))
   # print(commands)
   # print(back_commands)
   subprocess.run(back_commands)
@@ -78,8 +86,8 @@ def transform(image, q):
                   [0, 0, 1, 0],
                   [0, 0, 0, 1]])
 
-  M = translation@r_x@r_y@r_z
-  return M@image
+  # M = translation@r_x@r_y@r_z
+  return None
 
 def register_by_frame(sample):
   g_truth_frame = labelled_frames[sample]
@@ -110,7 +118,9 @@ labelled = ['010918L',  '013018L', '013118S',  '021218S',  '022415',  '031317L',
 ,'010918S',  '013018S',  '021015',   '022318L',  '022618',  '031317T',  '031616',  '032318a',  '032318d',  '040417',  '041017',	'043015',	'050318S', '051718L',  '052418L',  '061715',  '083115',
 '012115',	 '013118L',  '021218L',  '022318S',  '030315',  '031516',   '031716',  '032318b',  '032818',   '040617',  '041318S',	'043018',	'050917', '052218L',  '052418S',  '062515',  '102617']
 
-# for l in labelled:
-#   register(l)
+# truth = util.read_vol('../data/labels/021218L/021218L_0180_all_brains.nii.gz')
+# seg = util.read_vol('../data/data/021218L_0180.nii.gz')
+# u = union(truth, seg)
+# dilation = dilate(u, '021218L')
 
-register(labelled[0])
+register('021218L')
--- time_course.py
+++ time_course.py
@@ -18,17 +18,17 @@ usable = ['010918L', '010918S', '012115', '013018L', '013018S', '013118L',
            '100317L', '101317', '102717', '110214', '110217L', '110217S', 
            '111017L', '111017S', '112614', '120717', '122115']
 
-greater_90 = ['021218L', '021218S', '022318L', '030315', '031615', '032318a',
+greater_95 = ['021218L', '021218S', '030315', '032318a',
               '032318b', '041318S', '043018', '050318S', '051718L', '052418S',
-              '053017', '053117L', '053117S', '060118', '061217', '062817S', 
-              '071717L', '071717S', '081315', '082117L', '082517L', '110217S',
-              '111017S']
+              '060118', '061217', '062817S', 
+              '071717L', '081315', '082117L', '082517L', '102717', '110217S',
+              '111017S', '112164']
 
 O2_TIME = {'021218L': [5, 5, 5, 5], '021218S': [5, 5, 5, 5], '030315': [10, 10, 10], '032318a': [5, 5, 5, 5], 
             '032318b': [5, 5, 5, 5], '041318S': [5, 5, 3, 5], '043018': [5, 10], '050318S': [5, 5, 5, 5],
             '051718L': [5, 5, 5, 5], '052418S': [5, 5, 5, 5], '060118': [10, 10, 10], '061217': [10, 10],
             '062817S': [5, 5, 5, 5], '071717L': [5, 5, 5, 5], '081315': [10, 10, 10], '082117L': [5, 5, 5, 5], 
-            '082517L': [5, 5, 5, 5], '102717': [5, 5, 5, 5], '110217S': [5, 5, 5, 5], '011017S': [5, 5, 5, 5],
+            '082517L': [5, 5, 5, 5], '102717': [5, 5, 5, 5], '110217S': [5, 5, 5, 5], '111017S': [5, 5, 5, 5],
             '112164': [10, 10, 10]}
 
 
@@ -72,12 +72,12 @@ def get_O2_frames(sample, num_frames):
 
 def timecourse_interpolated(sample):
   good_frames = GOOD_FRAMES[sample]
-  frames = os.listdir('../data/raw/raw/{}'.format(sample))
+  frames = os.listdir('../data/raw/{}'.format(sample))
   signal_good = np.empty(len(frames))
   signal_good[:] = np.nan
   for frame in good_frames:
     predict = util.read_vol('../data/data/{}_{}.nii.gz'.format(sample, get_frame_num(frame)))
-    scan = util.read_vol('../data/raw/raw/{}/{}_{}.nii.gz'.format(sample, sample, get_frame_num(frame)))
+    scan = util.read_vol('../data/raw/{}/{}_{}.nii.gz'.format(sample, sample, get_frame_num(frame)))
     brain = scan[np.nonzero(predict)]
     avg_signal = np.mean(brain)
     signal_good[frame] = avg_signal
@@ -88,9 +88,9 @@ def timecourse_interpolated(sample):
     p = points[i]
     try:
       next_val = signal_good[p+1]
-      i = 1
+      j = 1
       while np.isnan(next_val):
-        i += 1
+        j += 1
         next_val = signal_good[p+i]
       avg_endpoints = np.mean([signal_good[p-1], next_val])
     except IndexError:
@@ -107,9 +107,9 @@ def timecourse_interpolated(sample):
   for i in range(len(O2)):
     color = colors[i%2]
     plt.axvline(x=O2[i], color=color)
-  savefile = './interpolated/less90/{}.png'.format(sample)
+  savefile = './interpolated/greater90/{}.png'.format(sample)
   plt.savefig(savefile)
   plt.close()
 
-for sample in greater_90:
+for sample in greater_95:
   timecourse_interpolated(sample)
